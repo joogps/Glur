5d96c9ce2336d1e573a2e31d5d0cc192d7e3ce75
João Gabriel <joogps@gmail.com>
2024-02-09 23:10:34 -0300

    Fix OS versions

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Glur",
-    platforms: [.iOS(.v13), .macOS(.v10_13), .watchOS(.v7), .tvOS(.v12), .visionOS(.v1)],
+    platforms: [.iOS(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
